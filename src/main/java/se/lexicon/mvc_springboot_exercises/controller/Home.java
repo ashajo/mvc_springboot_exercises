f2564c1c032bd1805ada87e96f068694eeac4dcb
asha joseph <ashajo183@gmail.com>
2021-04-16 20:13:51 +0200

    Created class "Home"in the controller package+crated "index"html page and added thymeleaf namespace and resources(Exercise 1)

---
+++ src/main/java/se/lexicon/mvc_springboot_exercises/controller/Home.java
@@ -0,0 +1,12 @@
+package se.lexicon.mvc_springboot_exercises.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+    @GetMapping("/index")
+    public String index(){
+        return "index";
+    }
+}
