4b3bca82cae35ea2c22b23c44c8bd9394aa558a3
asha joseph <ashajo183@gmail.com>
2021-04-16 20:03:47 +0200

    Initial commit

---
+++ src/test/java/se/lexicon/mvc_springboot_exercises/MvcSpringbootExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.mvc_springboot_exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcSpringbootExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
